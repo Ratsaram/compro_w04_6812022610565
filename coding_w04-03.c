0c4f4bd20a9edfb2952af94a42af3d1169c99abe
Ratsaran <s6812022610565@email.kmutnb.ac.th>
2025-07-23 15:33:07 +0700

    Add files via upload

---
+++ coding_w04-03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char nameInitial = 'P';
+    int age = 18;
+    float weight = 104.5;
+    char gender[] = "male";
+
+    printf("nameInitial: %c\n", nameInitial);
+    printf("age: %d\n", age);
+    printf("weight: %f\n", weight);
+    printf("gender: %s\n", gender);
+    return 0;
+
+}
